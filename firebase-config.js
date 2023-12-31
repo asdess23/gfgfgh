1c3d01899bd92f4979e28cdc42dfa30d117a40d8
asdess23 <155285066+asdess23@users.noreply.github.com>
2023-12-31 11:20:48 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQ_TzabAifK86AuVt8M8s-L7DM0M7Yvg0",
-  authDomain: "vgfh-a2cdf.firebaseapp.com",
-  databaseURL: "https://vgfh-a2cdf-default-rtdb.firebaseio.com",
-  projectId: "vgfh-a2cdf",
-  storageBucket: "vgfh-a2cdf.appspot.com",
-  messagingSenderId: "228425612058",
-  appId: "1:228425612058:web:ed25b61b2c513c78ad8aa3"
+  apiKey: "AIzaSyDMs06w3aoIqpr3KVebpia47xD-Jf71RbI",
+  authDomain: "gffgy-a560b.firebaseapp.com",
+  databaseURL: "https://gffgy-a560b-default-rtdb.firebaseio.com",
+  projectId: "gffgy-a560b",
+  storageBucket: "gffgy-a560b.appspot.com",
+  messagingSenderId: "599936211313",
+  appId: "1:599936211313:web:91d5f9b2b36c3460eebed0"
 };
